2f15699738a567c3df937f2fc7b2b8f975c6c47b
SainiManisha123 <33444857+SainiManisha123@users.noreply.github.com>
2024-09-09 22:24:29 +0530

    Delete gst/src/main/java/com/gst directory

--- gst/src/main/java/com/gst/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gst.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.gst.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- gst/src/main/java/com/gst/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gst.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.gst.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
